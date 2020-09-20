f3d38c56ac02de45626bc202eed9de0b2993a37b
sanjeeb kc <68530888+kcsanjeeb@users.noreply.github.com>
2020-09-20 14:52:12 +0545

    multi_photo

--- multi-photo/multi-photo.js
+++ multi-photo/multi-photo.js
@@ -1,33 +1,30 @@
-$(document).ready(function() {
-  if (window.File && window.FileList && window.FileReader) {
-    $("#files").on("change", function(e) {
-      var files = e.target.files,
-        filesLength = files.length;
-      for (var i = 0; i < filesLength; i++) {
-        var f = files[i]
-        var fileReader = new FileReader();
-        fileReader.onload = (function(e) {
-          var file = e.target;
-          $("<span class=\"pip\">" +
-            "<img class=\"imageThumb\" src=\"" + e.target.result + "\" title=\"" + file.name + "\"/>" +
-            "<br/><span class=\"remove\">Remove image</span>" +
-            "</span>").insertAfter("#files");
-          $(".remove").click(function(){
-            $(this).parent(".pip").remove();
+  
+  $(document).ready(function() {
+    if (window.File && window.FileList && window.FileReader) {
+      $("#files").on("change", function(e) {
+        var files = e.target.files,
+          filesLength = files.length;
+        for (var i = 0; i < filesLength; i++) {
+          var f = files[i]
+          var fileReader = new FileReader();
+          fileReader.onload = (function(e) {
+            var file = e.target;
+            $("<div class=\"container  pip \">"+
+              "<img class=\"imageThumb img-thumbnail\" src=\""  + e.target.result + "\" title=\"" + file.name + "\"/>"+
+                "<span class=\"middle remove\">"+
+              "<button type=\"button\" class=\"btn btn-danger\">Delete</button>"+
+                "</span>"+
+              "</div>").insertAfter("#files");
+            $(".remove").click(function(){
+              $(this).parent(".pip").remove();
+            });
+            
+     
           });
-          
-          // Old code here
-          /*$("<img></img>", {
-            class: "imageThumb",
-            src: e.target.result,
-            title: file.name + " | Click to remove"
-          }).insertAfter("#files").click(function(){$(this).remove();});*/
-          
-        });
-        fileReader.readAsDataURL(f);
-      }
-    });
-  } else {
-    alert("Your browser doesn't support to File API")
-  }
-});
+          fileReader.readAsDataURL(f);
+        }
+      });
+    } else {
+      alert("Your browser doesn't support to File API")
+    }
+  });
